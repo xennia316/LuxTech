b0af64950b6dc961b87fed2bda9dc3281d9c4340
xennia316 <xennia316@gmail.com>
2022-02-14 17:58:21 +0100

    if-else satements

---
+++ week1/conditionals.js
@@ -0,0 +1,25 @@
+/**
+ * by @xennia316
+ */
+
+let age = 79;
+
+if(age >= 15 && age <= 80 || age != 79)
+{
+    console.log("Go to club");
+}
+else if(age == 80){
+    console.log("Sleep now!");
+}
+else
+    console.log("Stay at home!");
+
+// double equal checks equality state
+// tripple equal checks datatype and values
+
+//Truthy and falsy check
+//Expressions that execute to false: 
+// NaN, undefined, false, 0,Empty strings.
+
+//Expressions that evaluate to true:
+//numbers other than 0, nonempty strings, nonempty objects
