7223ccf81befa5409a520b6ce77bfcbaf19a7b2f
xennia316 <xennia316@gmail.com>
2022-02-14 03:20:08 +0100

    using the console and writing comments

---
+++ week1/intro.js
@@ -0,0 +1,11 @@
+/**
+ * by @xennia316
+ */
+
+// Console on JS
+console.log("Hello World!");
+
+// Comments
+// Commenting in JavScript and programming is very important.
+// Comments are lines of code that are never executed and can help make a code more descriptive properties
+
