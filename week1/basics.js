023c194da10447ca44e4edf0b2c7a14d63c6f3ee
xennia316 <xennia316@gmail.com>
2022-02-14 03:00:16 +0100

    intro and functions of js

---
+++ week1/basics.js
@@ -0,0 +1,6 @@
+/**
+ * By Sonia Amehmbo
+ */
+
+// JavaScript is a cross-platform, object oriented scripting language used to make webpages interactive. I is used to perfoem complex actions like clicking a btton to make something appear, submitting forms and data, or handling popus in web page. JavaScript can perform DOMS(Document Oriented Model) manipulation.
+// It also has advanced server side versons such as Nodejs and Deno. These are used to do complex tasks that the client can't eg. storing data to databases, dowloading files, file manipulation.
