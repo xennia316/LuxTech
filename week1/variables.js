042b370133e9e4a8f70f1d93cc44302aedae59b7
xennia316 <xennia316@gmail.com>
2022-02-14 03:19:30 +0100

    different types of variables

---
+++ week1/variables.js
@@ -0,0 +1,23 @@
+/**
+ * by @xennia316
+ */
+// A variable is a symbolic name for your values
+
+// Three types of declaration
+// var
+//  var ten;
+//  var ten = 10;
+
+// //  let secction
+// let ten;
+// let ten = 10;
+
+// // const section
+// const ten; 
+// const ten = 10;
+
+// console.log(ten);
+// // Const always has to be initialised
+let bon;
+bon = "Bonvic";
+console.log(bon);
