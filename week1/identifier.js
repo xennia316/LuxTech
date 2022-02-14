ffdeceb7015e116fb76f6e67c4855635c2204937
xennia316 <xennia316@gmail.com>
2022-02-14 03:20:34 +0100

    Identifier types and syntaxes

---
+++ week1/identifier.js
@@ -0,0 +1,18 @@
+/**
+ * by @xennia316
+ */
+// An identifier must start with a letter, underscore(_), or dollar sign($).
+// subsequent characters can also be digits (0 - 9)
+// JS is case sensitive so you can use a - z or A -Z.
+// Also you can use unicode letters 
+
+let $money = "money";
+console.log($money);
+
+let _money = "Money";
+console.log(_money);
+
+let money = "MONEY";
+console.log(money);
+
+// constants cannot be reassigned
