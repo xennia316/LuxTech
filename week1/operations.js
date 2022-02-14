a4528bf09cd751f4072b81962440319e7e0119bd
xennia316 <xennia316@gmail.com>
2022-02-14 18:00:07 +0100

    Operations and syntaxing

---
+++ week1/operations.js
@@ -0,0 +1,34 @@
+/**
+ * by @xennia316
+ */
+//  +, -, *, /.
+
+//Addition (+)
+console.log(2 +2);
+//Subtraction (-)
+console.log(2 -2);
+//Multiplication (*)
+console.log(2 * 2);
+// DIvision (/)
+console.log(2/2);
+
+//Operators 
+//We have been using the assignment operator (=)
+
+//Addition assignment operator.
+let bon = 10;
+bon += 20;
+console.log(bon);
+
+// Subtraction assignment
+let Bon = 50;
+Bon -= bon;
+console.log(Bon);
+
+// Remainder assignement
+let rem = 9;
+rem %= Bon;
+console.log(rem);
+
+// multiplication assignment also exists
+// Division assignment also exists
